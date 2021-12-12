ba1116a79b5258851240d47948a122adf488ac27
Egydio Pacheco <egydio.neto@usp.br>
2021-12-12 09:50:46 -0300

    alias and new file

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: epacheco <epacheco@student.42sp.org.       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/28 19:15:19 by epacheco          #+#    #+#             */
-/*   Updated: 2021/12/10 19:08:34 by epacheco         ###   ########.fr       */
+/*   Updated: 2021/12/12 02:00:37 by epacheco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <unistd.h>
 # define REGULAR "regular"
 # define POINTER "pointer"
+# define STD_OUTPUT 1
 # define UPPER_HEXA "UPPER"
 # define UPPER_SEQUENCE "0123456789ABCDEF"
 # define LOWER_SEQUENCE "0123456789abcdef"
@@ -41,6 +42,7 @@ int		ft_print_p_symbol(void);
 int		ft_print_integer(va_list args);
 int		ft_print_hexa(va_list args, char *upper);
 int		ft_num_len(long long int nb, int base, char *flag);
+int		ft_num_len_unsigned(unsigned long long int nb, int base, char *flag);
 int		ft_putnbr_unsigned(unsigned int nb);
 void	ft_putnbr_hexa(unsigned long long nbr, const char *base);
 
